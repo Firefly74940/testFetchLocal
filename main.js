97ff2337d6f47132cfb8ce579ea84b105a6bd6d4
Firefly74940 <guiot.sidney@gmail.com>
2021-05-14 16:39:24 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+async function CheckUrl_Internal(url) {
+    try {
+        let response = await fetch(url);
+        let data = await response.text();
+        console.log('success:"' + url + '"', data);
+    } catch (error) {
+        console.error('failed:"' + url + '"', error);
+    }
+}
+async function CheckUrl(url) {
+    const pres = ['', 'http://', 'https://'];
+    const posts = ['', ':80', ':8080', ':4200', ':5000'];
+    for (const pre of pres) {
+        for (const post of posts) {
+            CheckUrl_Internal(pre + url + post)
+        }
+    }
+
+}
+
+CheckUrl("192.168.1.1");
+CheckUrl("localhost");
+CheckUrl("192.168.0.1");
